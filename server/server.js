08c66441040064b67f7dd6ce671a35b965fa08cf
Mateusz <mateusz.trajdos@gmail.com>
2019-07-01 10:05:25 +0100

    Fixed required error on the server side

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import path from 'path';
+const path = require('path');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
